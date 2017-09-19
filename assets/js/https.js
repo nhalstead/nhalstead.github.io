adcd113daacc718e79b6695c543b4ac23970dcf1
Noah Halstead <nhalstead@users.noreply.github.com>
2017-09-19 12:45:42 -0400

    Update https.js

--- assets/js/https.js
+++ assets/js/https.js
@@ -1,1 +1,2 @@
-if (window.location.protocol == "http:") window.location.href = "https" + window.location.href.slice(4);
+if (window.location.protocol == "http:") { window.location.href = "https" + window.location.href.slice(4); }
+else { console.log("You are in HTTPS, Good Job!"); }
