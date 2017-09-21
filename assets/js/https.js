667c8423c8aaa3ea0007716e30c758297ff20536
Noah Halstead <nhalstead@users.noreply.github.com>
2017-09-21 12:52:09 -0400

    Update https.js

--- assets/js/https.js
+++ assets/js/https.js
@@ -1,2 +1,5 @@
 if (window.location.protocol == "http:") { window.location.href = "https" + window.location.href.slice(4); }
 else { console.log("You are in HTTPS, Good Job!"); }
+
+//var loc = window.location.href+'';
+//if (loc.indexOf('http://')==0){ window.location.href = loc.replace('http://','https://'); }
